690e71a06dfa92738f7557ea531c62bd866a48dd
leske42 <mria.marika.huszr@gmail.com>
2026-01-09 20:20:38 +0100

    feat: add memcmp

---
+++ ft_memcmp.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhuszar <mhuszar@student.42vienna.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/06 17:10:11 by mhuszar           #+#    #+#             */
+/*   Updated: 2026/01/09 20:22:18 by mhuszar          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	__attribute__((noinline))
+	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	int	result;
+
+	__asm__ volatile (
+		"cld; repe cmpsb;"
+		"movzbl -1(%%rsi), %%eax;"
+		"movzbl -1(%%rdi), %%ecx;"
+		"sub %%ecx, %%eax;"
+		: "=a" (result)
+		: "S" (s1), "D" (s2), "c" (n)
+		: "cc", "flags"
+	);
+	return (result);
+}
+
+// #include<unistd.h>
+// #include<string.h>
+// #include<stdio.h>
+// int	main(void)
+// {
+// 	//printf("%d\n", memcmp("apple", "apple", 5));
+// 	//printf("%d", ft_memcmp("apple", "apple", 5));
+// 	printf("%d\n", memcmp("\xff\xaa\x12", "\xff\xaa\xde\x12MACOS", 4));
+// 	printf("%d\n", ft_memcmp("\xff\xaa\x12", "\xff\xaa\xde\x12MACOS", 4));
+// 	return (0);
+// }
