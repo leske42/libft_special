3b0e2e39d17d090b880796ddde327f9f37ad537d
leske42 <mria.marika.huszr@gmail.com>
2026-01-18 17:10:51 +0100

    fix(libasm): check for null byte in strcmp loop

--- libasm/ft_strcmp.s
+++ libasm/ft_strcmp.s
@@ -2,10 +2,16 @@ global ft_strcmp
 
 section .text
 ft_strcmp:
-    xor rax, rax
-    mov rcx, -1
     cld
-    repe cmpsb
+.loop:
+    cmpsb
+    jnz .done
+    cmp byte [-1 + rdi], 0
+    jz .done
+    cmp byte [-1 + rsi], 0
+    jz .done
+    jmp .loop
+.done:
     movzx rax, byte [-1 + rdi]
     movzx rcx, byte [-1 + rsi]
     sub rax, rcx
--- libasm/main.c
+++ libasm/main.c
@@ -6,7 +6,7 @@
 /*   By: mhuszar <mhuszar@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/18 14:41:11 by mhuszar           #+#    #+#             */
-/*   Updated: 2026/01/18 16:02:48 by mhuszar          ###   ########.fr       */
+/*   Updated: 2026/01/18 16:31:30 by mhuszar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,15 @@ int     ft_strcmp(const char *s1, const char *s2);
 
 int main(void)
 {
+    /*STRLEN TEST*/
     char *str = "tralalalala";
     printf("Length of %s is: %zu\n", str, ft_strlen(str));
+
+    /*STRCMP TEST*/
     char *str2 = "traf";
-    printf("Difference of %s and %s is: %d\n", str, str2, ft_strcmp(str2, str2));
-    // printf("Difference per original strcmp function: %d\n", strcmp(str, str2));
+    printf("Difference of %s and %s is: %d\n", str, str2, ft_strcmp(str, str2));
+    printf("Difference per original strcmp function: %d\n", strcmp(str, str2));
+    printf("Difference of %s and %s is: %d\n", str2, str2, ft_strcmp(str2, str2));
+    printf("Difference per original strcmp function: %d\n", strcmp(str2, str2));
     return (0);
 }
