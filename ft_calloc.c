9a152d3f4883929d1ba3d376d7d3ad96c93eff50
leske42 <mria.marika.huszr@gmail.com>
2026-02-15 16:12:46 +0100

    refactor(calloc): move calls out of asm block

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: mhuszar <mhuszar@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 11:36:23 by mhuszar           #+#    #+#             */
-/*   Updated: 2026/01/12 21:54:14 by mhuszar          ###   ########.fr       */
+/*   Updated: 2026/02/15 16:13:27 by mhuszar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,40 @@
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	void	*result;
+	size_t	total_size;
 
 	__asm__ volatile (
 		"xor %%rdx, %%rdx; mulq %%rdi; jno 1f;"
-		"xor %%rax, %%rax; jmp 2f;"
-		"1: push %%rax; push %%rcx; mov %%rax, %%rdi; call malloc;"
-		"pop %%rcx; pop %%rsi; test %%rax, %%rax; jz 2f;"
-		"push %%rax; mov %%rax, %%rdi; call *%%rcx; pop %%rax;"
-		"2: "
-		: "=a" (result)
-		: "D"(nmemb), "a"(size), "c" (ft_bzero)
+		"xor %%rax, %%rax;"
+		"1: "
+		: "=a" (total_size)
+		: "D"(nmemb), "a"(size)
 		: "memory", "cc", "flags", "rdx", "rsi"
 	);
+	if (total_size)
+		result = malloc(total_size);
+	if (result)
+		ft_bzero(result, total_size);
 	return (result);
 }
+
+// void	*ft_calloc(size_t nmemb, size_t size)
+// {
+// 	void	*result;
+
+// 	__asm__ volatile (
+// 		"xor %%rdx, %%rdx; mulq %%rdi; jno 1f;"
+// 		"xor %%rax, %%rax; jmp 2f;"
+// 		"1: push %%rax; push %%rcx; mov %%rax, %%rdi; call malloc;"
+// 		"pop %%rcx; pop %%rsi; test %%rax, %%rax; jz 2f;"
+// 		"push %%rax; mov %%rax, %%rdi; call *%%rcx; pop %%rax;"
+// 		"2: "
+// 		: "=a" (result)
+// 		: "D"(nmemb), "a"(size), "c" (ft_bzero)
+// 		: "memory", "cc", "flags", "rdx", "rsi"
+// 	);
+// 	return (result);
+// }
 
 #include <string.h>
 int	main(void)
