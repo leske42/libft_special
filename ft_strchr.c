256b90ab8656e07b5d339942a5ac89a896b5d81a
leske42 <mria.marika.huszr@gmail.com>
2026-02-16 22:12:00 +0100

    feat(libft): add strchr

---
+++ ft_strchr.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mhuszar <mhuszar@student.42vienna.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/05 16:02:25 by mhuszar           #+#    #+#             */
+/*   Updated: 2026/02/16 22:11:37 by mhuszar          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	void	*result;
+
+	__asm__ volatile (
+		"cld; mov $-1, %%rcx; repne scasb;"
+		"xor %%rsi, %%rsi;"
+		"cmpb -1(%%rdi), %%al; jne 1f;"
+		"lea -1(%%rdi, %%rsi, 1), %%rsi; 1:"
+		: "=S" (result)
+		: "D" (s), "a" (c)
+		: "cc", "flags", "rcx"
+	);
+	return (result);
+}
+
+// #include<string.h>
+// int	main(void)
+// {
+// 	char *alma;
+// 	int a = 97;
+// 	int l = 108;
+// 	int n = 110;
+// 	alma = "alma";
+// 	printf("%s\n", ft_strchr(alma, a));
+// 	char s[] = "tripouille";
+// 	printf("%s\n", ft_strchr(s, 'i' + 256));
+// 	printf("%s", strchr(s, 'i' + 256));
+// 	return (0);
+// }
