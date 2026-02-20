38a4616765028cd3de21fd7ad0693b889afbd986
leske42 <mria.marika.huszr@gmail.com>
2026-02-20 21:38:06 +0100

    style(strtrim): use lea for adjusting source pointer on move

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: mhuszar <mhuszar@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 17:20:18 by mhuszar           #+#    #+#             */
-/*   Updated: 2026/02/20 21:29:47 by mhuszar          ###   ########.fr       */
+/*   Updated: 2026/02/20 21:36:48 by mhuszar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,7 @@ char	*ft_strtrim(char const *s1, char const *set)
 		"1: dec %%rdx; js 2f; lodsb;"
 		"movq (%%rsp), %%rcx; movq 8(%%rsp), %%rdi; cld; repne scasb; jz 0b;"
 		"test %%r8, %%r8; jnz 2f; mov %%rsi, %%rbx; dec %%rbx;"
-		"add %%rdx, %%rsi; sub $2, %%rsi; not %%r8; jmp 0b;"
+		"lea -2(%%rsi, %%rdx, 1), %%rsi; not %%r8; jmp 0b;"
 		"2: add $16, %%rsp; add $2, %%rdx; cld;"
 		: "=b" (begin), "=d" (len)
 		: "c" (ft_strlen(set)), "d" (ft_strlen(s1) + 1), "S" (s1), "D" (set)
