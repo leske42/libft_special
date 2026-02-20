1febd8d3e9a93fd4d41a9dd66c5d427610f104a3
leske42 <mria.marika.huszr@gmail.com>
2026-02-20 21:00:54 +0100

    refactor(strtrim): remove code duplication and set norm

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: mhuszar <mhuszar@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 17:20:18 by mhuszar           #+#    #+#             */
-/*   Updated: 2026/02/20 20:50:39 by mhuszar          ###   ########.fr       */
+/*   Updated: 2026/02/20 21:00:22 by mhuszar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,18 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	char *begin;
-	size_t len;
-	char *res;
-	
+	char	*begin;
+	size_t	len;
+	char	*res;
+
 	__asm__ volatile (
-		"push %%rdi; push %%rcx; cld;"
-		"0: test %%rdx, %%rdx; jz 2f; dec %%rdx; lodsb; movq (%%rsp), %%rcx; movq 8(%%rsp), %%rdi; repne scasb; jz 0b;" //run until first marker point
-		"mov %%rsi, %%rbx; dec %%rbx; add %%rdx, %%rsi; dec %%rsi; dec %%rsi;"
-		"1: test %%rdx, %%rdx; jz 2f; dec %%rdx; std; lodsb; movq (%%rsp), %%rcx; movq 8(%%rsp), %%rdi; cld; repne scasb; jz 1b;"
-		"2: add $16, %%rsp; inc %%rdx; inc %%rdx;"
+		"push %%rdi; push %%rcx; cld; xor %%r8, %%r8;"
+		"0: test %%r8, %%r8; jz 1f; std;"
+		"1: test %%rdx, %%rdx; jz 2f; dec %%rdx; lodsb;"
+		"movq (%%rsp), %%rcx; movq 8(%%rsp), %%rdi; cld; repne scasb; jz 0b;"
+		"test %%r8, %%r8; jnz 2f; mov %%rsi, %%rbx; dec %%rbx;"
+		"add %%rdx, %%rsi; sub $2, %%rsi; not %%r8; jmp 0b;"
+		"2: add $16, %%rsp; add $2, %%rdx; cld;"
 		: "=b" (begin), "=d" (len)
 		: "c" (ft_strlen(set)), "d" (ft_strlen(s1) + 1), "S" (s1), "D" (set)
 		: "rax", "cc", "flags"
@@ -36,10 +38,10 @@ char	*ft_strtrim(char const *s1, char const *set)
 	return (res);
 }
 
-int	main(void)
-{
-	printf("%s\n", ft_strtrim("/?***ubuntu//software\t**\t", "\t"));
-	printf("%s\n", ft_strtrim("ubuntu//software", "ubntsofwear"));
-	printf("%s\n", ft_strtrim("ubuntusoftware", "ubntsofwear"));
-	return (0);
-}
+// int	main(void)
+// {
+// 	printf("%s\n", ft_strtrim("/?***ubuntu//software\t**\t", "\t"));
+// 	printf("%s\n", ft_strtrim("ubuntu//software", "ubntsofwear"));
+// 	printf("%s\n", ft_strtrim("ubuntusoftware", "ubntsofwear"));
+// 	return (0);
+// }
