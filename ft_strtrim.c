a4251fb28ecdb35ee3e4224969af2fbd7595db54
leske42 <mria.marika.huszr@gmail.com>
2026-02-20 21:13:19 +0100

    docs(strtrim): document logic flow

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,12 +6,27 @@
 /*   By: mhuszar <mhuszar@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 17:20:18 by mhuszar           #+#    #+#             */
-/*   Updated: 2026/02/20 21:00:22 by mhuszar          ###   ########.fr       */
+/*   Updated: 2026/02/20 21:16:34 by mhuszar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <string.h>
+
+/*
+R8 will be the flag if we are currently in the iteration of looking fwd or
+bckwd. Therefore we always set DF based on R8 content. Otherwise setup is
+easy: we load one char from the string, try to find it in SET string with
+REPNE SCASB, as long as we find it we keep going. RDX starts with storing
+full S1 string len, while we keep going we keep subtracting it, at the end
+will give us the len to malloc. As soon as we find one char not in the SET,
+we first save RSI to RBX as the BEGIN PTR, then we add RDX content to RSI
+and like this jump to end end of the string. We set R8 so now me move
+backwards. Otherwise logic is same for both periods.
+For clean setup I had no choice but to leave RBX clobbered, but compiler
+knows it was used based on parameters and will make sure to save/restore
+around the block. Also needed to add this unit to NOREDZONE because of stack
+usage.
+*/
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
@@ -29,9 +44,8 @@ char	*ft_strtrim(char const *s1, char const *set)
 		"2: add $16, %%rsp; add $2, %%rdx; cld;"
 		: "=b" (begin), "=d" (len)
 		: "c" (ft_strlen(set)), "d" (ft_strlen(s1) + 1), "S" (s1), "D" (set)
-		: "rax", "cc", "flags"
+		: "rax", "cc", "flags", "r8"
 	);
-	printf("LEN IS: %zu, STARTING COPYING FROM: %s\n", len, begin);
 	res = malloc(len);
 	if (res)
 		ft_strlcpy(res, begin, len);
