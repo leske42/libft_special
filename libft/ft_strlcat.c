c90c8b6bed84ce2b57fac2045971e2d3033ebdd8
leske42 <mria.marika.huszr@gmail.com>
2026-02-23 17:07:43 +0100

    feat(libft): add strlcat

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,59 +6,47 @@
 /*   By: mhuszar <mhuszar@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/30 20:56:38 by mhuszar           #+#    #+#             */
-/*   Updated: 2025/10/02 23:03:28 by mhuszar          ###   ########.fr       */
+/*   Updated: 2026/02/23 17:07:28 by mhuszar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+//now i remember the main issue with these functions was not to make them work,
+//but to provide always the stupid return value specified
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	size_t	dest_len;
-	size_t	src_len;
-	size_t	counter;
-	size_t	dest_proxy;
+	size_t	res;
 
-	counter = 0;
-	dest_len = ft_strlen(dst);
-	src_len = ft_strlen(src);
-	dest_proxy = ft_strlen(dst);
-	if (size <= dest_len)
-		return (src_len + size);
-	while (size > 0)
-	{
-		while (src[counter] && dest_proxy < size - 1)
-		{
-			dst[dest_proxy] = src[counter];
-			counter++;
-			dest_proxy++;
-		}
-		break ;
-	}
-	dst[dest_proxy] = '\0';
-	return (src_len + dest_len);
+	__asm__ volatile (
+		"cmp %%rcx, %%rdx; jg 6f; add %%rdx, %%rax; dec %%rcx;"
+		"sub %%rdx, %%rcx; add %%rdx, %%rdi; rep movsb; jmp 7f;"
+		"6: add %%rcx, %%rax; 7:"
+		: "=a" (res)
+		: "D" (dst), "S" (src), "c" (size), "a" (ft_strlen(src)), \
+"d" (ft_strlen(dst))
+		:
+	);
+	return (res);
 }
 
-// apple
-// hehe
-// 8
+// #include<stdio.h>
+// #include <bsd/string.h>
+// //link with -lbsd here also
+// int	main(void)
+// {
+// 	char dst[15] = "apple";
+// 	char dst2[15] = "apple";
+// 	char src[4] = "hehe";
+// 	printf("%zu\n", strlcat(dst2, src, 2));
+// 	printf("%zu\n", ft_strlcat(dst, src, 2));
+// 	printf("STRLCAT COPIED: %s\n", dst2);
+// 	printf("FT_STRLCAT COPIED: %s\n", dst);
 
-// happlehee
-
-// applehe
-
-
-#include<stdio.h>
-#include <bsd/string.h>
-int	main(void)
-{
-	char dst[15] = "apple";
-	char src[4] = "hehe";
-	// printf("%zu\n", ft_strlcat(dst, src, 2));
-	// printf("%d", strlcat(dst, src, 2));
-	strlcat(dst, src, 7);
-	printf("%s", dst);
-	//ft_strlcat(dst, src, 6);
-	//printf("%s", dst);
-	return (0);
-}
+// 	//if i change the order of these 2 the result is different??
+// 	printf("%zu\n", strlcat(dst2, src, 9));
+// 	printf("%zu\n", ft_strlcat(dst, src, 9));
+// 	printf("STRLCAT COPIED: %s\n", dst2);
+// 	printf("FT_STRLCAT COPIED: %s\n", dst);
+// 	return (0);
+// }
