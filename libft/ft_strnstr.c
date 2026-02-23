81b1c5a557a7fd10f9682280c3f4dc8e44221572
leske42 <mria.marika.huszr@gmail.com>
2026-02-23 16:26:28 +0100

    feat(libft): add strnstr

--- libft/ft_strnstr.c
+++ libft/ft_strnstr.c
@@ -3,47 +3,57 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mhuszar <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: mhuszar <mhuszar@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/06 17:10:34 by mhuszar           #+#    #+#             */
-/*   Updated: 2023/09/06 17:10:37 by mhuszar          ###   ########.fr       */
+/*   Updated: 2026/02/23 16:26:11 by mhuszar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "libft.h"
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
-	size_t	counter_a;
-	size_t	counter_b;
+	char	*res;
 
-	counter_a = 0;
-	counter_b = 0;
-	if (little[0] == 0)
-		return ((char *)big);
-	while (big[counter_a] != 0 && little[counter_b] != 0)
-	{
-		while (big[counter_a] == little[counter_b])
-		{
-			counter_a++;
-			counter_b++;
-			if (counter_a > len)
-				return (0);
-			else if (little[counter_b] == '\0')
-				return ((char *)big + counter_a - counter_b);
-		}
-		counter_a = (counter_a - counter_b) + 1;
-		counter_b = 0;
-	}
-	return (0);
+	__asm__ volatile ("mov %%rdi, %%rdx; mov %%rsi, %%rax; inc %%rcx;"
+		"mov %%rcx, %%r8; cmpb $0, (%%rdi); jz 3f; cld;"
+		"1: repe cmpsb; cmpb $0, -1(%%rdi); jz 3f; cmpb $0, -1(%%rsi); jz 2f;"
+		"test %%rcx, %%rcx; jz 2f; mov %%rdx, %%rdi; inc %%rax;"
+		"mov %%rax, %%rsi; dec %%r8; mov %%r8, %%rcx; jmp 1b;"
+		"2: xor %%rax, %%rax; 3:"
+		: "=a" (res)
+		: "S" (big), "D" (little), "c" (len)
+		: "rdx", "r8", "cc"
+	);
+	return (res);
 }
-/*
-int	main(void)
-{
-	char a[] = "12a45ababc90";
-	char b[] = "abc";
-	char *c = ft_strnstr(a, b, 12);
-	printf("%s\n", c);
-	c = ft_strnstr(a, b, 9);
-	printf("%s", c);
-	return (0);
-}*/
+
+// #include <string.h>
+// //requires -lbsd
+// char *strnstr(const char *big, const char *little, size_t len);
+// int	main(void)
+// {
+// 	char a[] = "12a45ababc90";
+// 	char b[] = "abc";
+// 	char *c = ft_strnstr(a, b, 12);
+// 	printf("%s\n", c);
+// 	c = strnstr(a, b, 12);
+// 	printf("%s\n", c);
+
+// 	c = ft_strnstr(a, b, 9);
+// 	printf("%s\n", c);
+// 	c = strnstr(a, b, 9);
+// 	printf("%s\n", c);
+
+// 	c = ft_strnstr("hehiahehhehe", "hehe", 12);
+// 	printf("%s\n", c);
+// 	c = strnstr("hehiahehhehe", "hehe", 12);
+// 	printf("%s\n", c);
+
+// 	c = ft_strnstr("hehiahehhehe", "hehe", 11);
+// 	printf("%s\n", c);
+// 	c = strnstr("hehiahehhehe", "hehe", 11);
+// 	printf("%s\n", c);
+// 	return (0);
+// }
