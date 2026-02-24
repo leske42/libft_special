0395648ddf7c3cbdee1f887f020d1890940604b2
leske42 <mria.marika.huszr@gmail.com>
2026-02-24 21:29:54 +0100

    refactor(libft): putnbr

--- libft/ft_putnbr_fd.c
+++ libft/ft_putnbr_fd.c
@@ -6,46 +6,40 @@
 /*   By: mhuszar <mhuszar@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/29 17:53:29 by mhuszar           #+#    #+#             */
-/*   Updated: 2024/02/18 19:31:16 by mhuszar          ###   ########.fr       */
+/*   Updated: 2026/02/24 21:53:50 by mhuszar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+//throws up everything on the stack in reverse order & prints
 void	ft_putnbr_fd(int nb, int fd)
 {
-	__asm__ volatile (
-		"movl %0, %%eax; movsx %%eax, %%rax; pushq %%rbx; xorq %%rbx, %%rbx;"
-		"cmp $-1, %%rax; jg 1f; imulq $-1, %%rax; movq $1, %%rdx;"
-		"movl %1, %%edi; sub $1, %%rsp; movq $45, (%%rsp);"
-		"movq %%rsp, %%rsi; pushq %%rax; movq $1, %%rax;"
-		"syscall; popq %%rax; add $1, %%rsp;"
-		"1:"
-		"xorq %%rdx, %%rdx; movq $10, %%rcx; divq %%rcx; addq $48, %%rdx;"
-		"pushq %%rdx; incq %%rbx; cmp $0, %%rax; jne 1b;"
-		"2:"
-		"movq $1, %%rax; movl %1, %%edi; movq %%rsp, %%rsi;"
-		"movq $1, %%rdx; syscall; decq %%rbx; popq %%rcx;"
-		"cmp $0, %%rbx; jne 2b; popq %%rbx;"
+	__asm__ volatile ("xor %%r8, %%r8; xor %%r9, %%r9;"
+		"movslq %%eax, %%rax; test %%rax, %%rax; jns 1f; neg %%rax; not %%r9;"
+		"1: xor %%rdx, %%rdx; movq $10, %%rcx; divq %%rcx; addq $48, %%rdx;"
+		"sub $1, %%rsp; movb %%dl, (%%rsp); inc %%r8; cmp $0, %%rax; jne 1b;"
+		"test %%r9, %%r9; jz 2f; sub $1, %%rsp; movb $45, (%%rsp); inc %%r8;"
+		"2: movq $1, %%rax; movq %%rsp, %%rsi; movq %%r8, %%rdx; syscall;"
+		"add %%r8, %%rsp;"
+		: "+a" (nb), "+D" (fd)
 		:
-		: "r" (nb), "r" (fd)
-		: "memory", "rdi", "rsi", "rax", "rdx", "cc", "rcx"
+		: "memory", "rsi", "rdx", "cc", "rcx", "r11", "r8", "r9"
 	);
 }
 
-/*
-int	main(void)
-{
-	//ft_putnbr_fd(2147483647, 1);
-	//ft_putchar_fd('\n', 1);
-	ft_putnbr_fd(-200, 1);
-	printf("\n");
-	ft_putnbr_fd(2147483647, 1);
-	printf("\n");
-	ft_putnbr_fd(-2147483648, 1);
-	//ft_putchar_fd('\n', 1);
-	//ft_putnbr_fd(-220, 1);
-	//ft_putchar_fd('\n', 1);
-	//ft_putnbr_fd(0, 1);
-	return (0);
-}*/
+// int	main(void)
+// {
+// 	//ft_putnbr_fd(2147483647, 1);
+// 	//ft_putchar_fd('\n', 1);
+// 	ft_putnbr_fd(-200, 1);
+// 	printf("\n");
+// 	ft_putnbr_fd(2147483647, 1);
+// 	printf("\n");
+// 	ft_putnbr_fd(-2147483648, 1);
+// 	//ft_putchar_fd('\n', 1);
+// 	//ft_putnbr_fd(-220, 1);
+// 	//ft_putchar_fd('\n', 1);
+// 	//ft_putnbr_fd(0, 1);
+// 	return (0);
+// }
