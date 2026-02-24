d22f4f980a6fd84bb451cdca46f2ee335a8cf2a7
leske42 <mria.marika.huszr@gmail.com>
2026-02-24 22:34:06 +0100

    fix(strlcat): switch to unsigned cmp condition

--- libft/ft_strlcat.c
+++ libft/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: mhuszar <mhuszar@student.42vienna.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/30 20:56:38 by mhuszar           #+#    #+#             */
-/*   Updated: 2026/02/23 20:06:30 by mhuszar          ###   ########.fr       */
+/*   Updated: 2026/02/24 22:39:28 by mhuszar          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,11 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	size_t	res;
 
 	__asm__ volatile ("mov %4, %%rax; cld;"
-		"cmp %%rcx, %%rdx; jg 6f; add %%rdx, %%rax; dec %%rcx;"
-		"sub %%rdx, %%rcx; add %%rdx, %%rdi; rep movsb; jmp 7f;"
+		"cmp %%rcx, %%rdx; jae 6f; add %%rdx, %%rax;"
+		"sub %%rdx, %%rcx; dec %%rcx; cmp %4, %%rcx; cmova %4, %%rcx;"
+		"add %%rdx, %%rdi; rep movsb; movb $0, (%%rdi); jmp 7f;"
+		// interestingly it seems like I can have comments in the code
+		// and norminette doesn't mind it
 		"6: add %%rcx, %%rax; 7:"
 		: "=a" (res), "+D" (dst), "+S" (src), "+c" (size)
 		: "r" (ft_strlen(src)), "d" (ft_strlen(dst))
@@ -37,6 +40,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 // 	char dst[15] = "apple";
 // 	char dst2[15] = "apple";
 // 	char src[4] = "hehe";
+// 	// char *src = "";
 // 	printf("%zu\n", strlcat(dst2, src, 2));
 // 	printf("%zu\n", ft_strlcat(dst, src, 2));
 // 	printf("STRLCAT COPIED: %s\n", dst2);
@@ -47,5 +51,6 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 // 	printf("%zu\n", ft_strlcat(dst, src, 9));
 // 	printf("STRLCAT COPIED: %s\n", dst2);
 // 	printf("FT_STRLCAT COPIED: %s\n", dst);
+
 // 	return (0);
 // }
